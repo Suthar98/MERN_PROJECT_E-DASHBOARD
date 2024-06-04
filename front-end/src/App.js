c02689722a444eab7ab7eede33a7bcbaf4d8f089
Suthar98 <sutharvictor10@gmail.com>
2024-06-04 18:52:25 +0530

    #5, routing in react app and do navigation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Nav from './nav';
 
 function App() {
   return (
     <div className="App">
-      Welcome
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Product Component</h1>} />
+          <Route path="/add" element={<h1>Product Component</h1>} />
+          <Route path="/update" element={<h1>Product Component</h1>} />
+          <Route path="/logout" element={<h1>Product Component</h1>} />
+          <Route path="/profile" element={<h1>Product Component</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
